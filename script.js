6363bd9f8139196d050ecb97fada1fc4425bebf2
Infalls <91087294+Infalls@users.noreply.github.com>
2022-07-12 22:02:05 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,1 @@
+<script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A9b6dd9dd3bda5dcb8ccceed45802648e814f1e8f8340110f877ae58e2e02d4cf&amp;width=500&amp;height=400&amp;lang=ru_RU&amp;scroll=true"></script>
